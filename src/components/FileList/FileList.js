8c8cd222930cfe993168885505bfbd256a7cbf36
gwyndol1ne <Dragon>
2023-09-29 23:00:16 +0300

    ScrollArea add

--- src/components/FileList/FileList.js
+++ src/components/FileList/FileList.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import File from '../File/File';
 import './FileList.css'
+import { ScrollArea } from '@mantine/core';
 
 const FileList = ({ files, remove }) => {
 
   return (
     <div className="fileList">
-      {files.map((file, index) => (
-        <File key={file.name} file={file} remove={remove} index={index}/>
+      <ScrollArea h={660} type="never" offsetScrollbars scrollbarSize={8} scrollHideDelay={0}>
+      {files.map((file, index) => (      
+        <File key={file.name} file={file} remove={remove} index={index}/>             
       ))}
+      </ScrollArea>
     </div>
   );
 };
